d8d21439cd3b39eb23e6040de1695cdbb1cba6f3
Felix Engelmann <fe-github@nlogn.org>
2026-04-07 18:16:59 -0400

    loop

--- apps/fcmcclerk/management/commands/scrape.py
+++ apps/fcmcclerk/management/commands/scrape.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from django.core.management.base import BaseCommand
 from apps.fcmcclerk.tasks import decide_next_scrape, scrape_detail, parse_page
 
@@ -6,7 +9,11 @@ class Command(BaseCommand):
     help = "Scrapes a FCMC case"
 
     def handle(self, *args, **options):
-        cno = decide_next_scrape()
-        pg = scrape_detail(cno)
-        if pg.return_code == 200:
-            parse_page(pg)
+        while True:
+            cno = decide_next_scrape()
+            logging.info("next case %s", cno)
+            pg = scrape_detail(cno)
+            if pg.return_code == 200:
+                logging.info("parse and add %s", pg)
+                parse_page(pg)
+            time.sleep(8)
