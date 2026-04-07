91fe82b6a9a219182ded777d392ef2e4be55b077
Felix Engelmann <fe-github@nlogn.org>
2026-04-07 11:06:12 -0400

    run scrape on command

--- apps/fcmcclerk/management/commands/scrape.py
+++ apps/fcmcclerk/management/commands/scrape.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
-from apps.fcmcclerk.tasks import decide_next_scrape
+from apps.fcmcclerk.tasks import decide_next_scrape, scrape_detail
 
 
 class Command(BaseCommand):
     help = "Scrapes a FCMC case"
 
     def handle(self, *args, **options):
-        decide_next_scrape()
+        cno = decide_next_scrape()
+        scrape_detail(cno)
