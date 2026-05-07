9bb84d2c717fe5f7300f9a00afe1267533d124ef
Felix Engelmann <fe-github@nlogn.org>
2026-05-07 08:57:59 -0400

    optionally allow scrape of specific cases

--- apps/fcmcclerk/management/commands/fcmcclerk.py
+++ apps/fcmcclerk/management/commands/fcmcclerk.py
@@ -3,14 +3,36 @@ import time
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from apps.fcmcclerk.tasks import scrape_detail, parse_page, scrape_generator
+from apps.fcmcclerk.tasks import scrape_detail, parse_page, scrape_generator, ScrapeInstruction
 
+
+def scrape_and_parse(instr: ScrapeInstruction):
+    pg = scrape_detail(instr)
+    if pg.return_code == 200:
+        logging.info("parse and add %s", pg)
+        try:
+            parse_page(pg)
+        except Exception as e:
+            logging.error("page %s could not be parsed: %s", pg, e.__repr__())
+            pg.return_code = 401
+            pg.save()
+
+    time.sleep(15)
 
 class Command(BaseCommand):
     help = "Scrapes a FCMC case"
 
+    def add_arguments(self, parser):
+        parser.add_argument("case_number", nargs="*", type=str)
+
     def handle(self, *args, **options):
         logging.info("start scraping")
+        scrape_cases = options.get("case_number",[])
+        if len(scrape_cases) > 0:
+            print("opt", scrape_cases)
+            for case_number in scrape_cases:
+                scrape_and_parse(ScrapeInstruction(case_number=case_number))
+            return
         while True:
             for cno in scrape_generator():
                 logging.info("next case %s", cno)
@@ -22,14 +44,5 @@ class Command(BaseCommand):
                     while datetime.now() < cno.earliest:
                         time.sleep(10)
                     break
-                pg = scrape_detail(cno)
-                if pg.return_code == 200:
-                    logging.info("parse and add %s", pg)
-                    try:
-                        parse_page(pg)
-                    except Exception as e:
-                        logging.error("page %s could not be parsed: %s", pg, e.__repr__())
-                        pg.return_code = 401
-                        pg.save()
 
-                time.sleep(15)
+                scrape_and_parse(cno)
