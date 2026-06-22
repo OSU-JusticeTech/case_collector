ae0543881b78d2adfead8257043aae9bf6bd1884
Felix Engelmann <fe-github@nlogn.org>
2026-06-22 15:05:06 -0400

    fields for extracted data

--- apps/attending/models.py
+++ apps/attending/models.py
@@ -8,5 +8,11 @@ class CheckinSheet(models.Model):
     taken_at = models.DateTimeField()
     file_hash = models.CharField(max_length=64, unique=True)
 
+    possible_start = models.DateField(null=True, blank=True)
+    possible_end = models.DateField(null=True, blank=True)
+
+    processed = models.JSONField(null=True)
+    validated = models.BooleanField(default=False)
+
     def __str__(self):
         return self.filename
